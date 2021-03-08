3f91382f635da77b1579f63ec2b208bfdf388203
Stefan <tf19683@bristol.ac.uk>
2021-03-08 15:24:43 +0000

    Moved the initial conditions into main and added the radius as a pass-through argument

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
 import numpy as np
-from scipy.integrate import *
+from scipy.integrate import solve_ivp
 from plot import *
 from methods import *
 
-# Initial Conditions
-x0 = 0
-phi0 = np.pi/2
-v0 = 0
-w0 = 0
-# Constant values
-m1 = 10
-m2 = 1
-l = 20
-mu = 0
-g = 9.793
-# Integration region
-t_0 = 0
-t_end = 5
-
 
 def main():
-
-    args = [m1, m2, l, mu, g]
+    args = [m1, m2, l, mu, g, r]
     initial_conds = [x0, phi0, v0, w0]
+    print("Integrating...")
 
-    sol = solve_ivp(diff_func_P, (t_0, t_end), initial_conds, method='BDF', args=([args]), dense_output=True)
+    sol = solve_ivp(diff_func_L5, (t_0, t_end), initial_conds, method='RK45', args=([args]), dense_output=True, max_step = 0.0001)
 
-    print("Integration success = {}, {}".format(sol.success,sol.message))
+    print("Integration success = {}, {}".format(sol.success, sol.message))
 
     plot_solution_panels(sol)
+    plot_spiral(sol, l, r)
+    plot_colour_spiral(sol, l, r)
 
 
 if __name__ == '__main__':
+    # Initial Conditions
+    x0 = 0
+    phi0 = 0
+    v0 = 0
+    w0 = 0
+    # Constant values
+    m1 = 250 # Heavier mass
+    m2 = 50  # Lighter mass
+    l = 20
+    mu = 0.5
+    g = 9.81
+    r = 1  # Radius of the "pulley"
+    # Integration region
+    t_0 = 0
+    t_end = 15
+
     main()
